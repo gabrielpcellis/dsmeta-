e06ecd22829a8c54675c1dccbe6e37c7795607bf
Gabriel Porcellis <gabrielporcellis@gmail.com>
2022-07-17 21:25:01 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (<>
-    <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-          <SalesCard />
-        </div>
-      </section>
-    </main>
-  </>)
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>)
 }
 
 export default App;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("SUCESSO")
+        toast.info("SMS enviado com sucesso");
     })
 }
 
